5cfdbf78db04f6d83531ffbf54ac3fcc0b0b94fd
Daniel Herrera <mayan.bridge.consulting@gmail.com>
2022-07-15 11:56:20 +0200

    build new version of website

---
+++ build/precache-manifest.4d6e6e179ed5736a679b2c078f736d55.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c472673ba776498b4513",
+    "url": "/static/js/main.37f19356.chunk.js"
+  },
+  {
+    "revision": "ef5a25257b2020ef0dc6",
+    "url": "/static/js/runtime~main.664399db.js"
+  },
+  {
+    "revision": "3c418b6d49e38bfa3258",
+    "url": "/static/js/2.4d9bb8a0.chunk.js"
+  },
+  {
+    "revision": "b61d7e105384ced4ef5b",
+    "url": "/static/js/3.66188ad7.chunk.js"
+  },
+  {
+    "revision": "280b767c2cab7d7bf20ba7573ea90397",
+    "url": "/static/media/Inherit.280b767c.png"
+  },
+  {
+    "revision": "4dc8fc0e630ec72e5cd30e43e629699a",
+    "url": "/static/media/Register.4dc8fc0e.png"
+  },
+  {
+    "revision": "adc50414354690216ece1dd3b48cae1d",
+    "url": "/static/media/Monitor.adc50414.png"
+  },
+  {
+    "revision": "a8b4c5d15608dfd23a2b348bf44ef632",
+    "url": "/static/media/Primary Logo.a8b4c5d1.png"
+  },
+  {
+    "revision": "5fbe9743312ab23bff492ecc6235a63e",
+    "url": "/static/media/appworks01.5fbe9743.png"
+  },
+  {
+    "revision": "96ba801e1295d80a8ef343c4241acdbb",
+    "url": "/index.html"
+  }
+];
